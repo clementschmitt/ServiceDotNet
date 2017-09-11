8375d228b209bfb70717d7212dfe431488fe9966
Schmitt <clement.flance@gmail.com>
2017-09-12 01:23:01 +0200

    Add solution and directories

---
+++ MonPremierServiceWCF/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MonPremierServiceWCF
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return $"valeur passé en paramètre : {value}";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public float GetPrice(string id)
+        {
+            switch (id)
+            {
+                case "Mario et lapins crétins":
+                    return 49.99f;
+                case "PS4":
+                    return 399.99f;
+                case "Assasins Creed Black Flag":
+                    return 29.99f;
+                default:
+                    return 99999999999999999.999f;
+            }
+        }
+    }
+}
