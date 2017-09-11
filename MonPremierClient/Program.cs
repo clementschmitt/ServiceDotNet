e7847d53ff50030f02d733318f62797305f65ec3
Schmitt <clement.flance@gmail.com>
2017-09-12 01:45:18 +0200

    More products

--- MonPremierClient/Program.cs
+++ MonPremierClient/Program.cs
@@ -10,10 +10,24 @@ namespace MonPremierClient
     {
         static void Main(string[] args)
         {
-            var client = new ServiceReference1.Service1Client();
-            Console.WriteLine(client.GetPrice("Mario et lapins crétins"));
+            var input = "";
 
-            Console.ReadLine();
+            var client = new ServiceReference1.Service1Client();
+
+            do
+            {
+                Console.WriteLine("Pour quel produit souhaitez-vous afficher le prix ?");
+                Console.WriteLine("1 - Mario et lapins crétins");
+                Console.WriteLine("2 - Assasins Creed Black Flag");
+
+                input = Console.ReadLine();
+
+                Console.WriteLine(input == "1" ? "Le prix du produit 'Mario et lapins crétins' est de " + client.GetPrice("Mario et lapins crétins").ToString() + "\n": 
+                                  input == "2" ? "Le prix du produit 'Assasins Creed Black Flag' est de " + client.GetPrice("Assasins Creed Black Flag").ToString() + "\n": "\n");
+
+            } while (input != "quit");
+
+            client.Close();
         }
     }
 }
